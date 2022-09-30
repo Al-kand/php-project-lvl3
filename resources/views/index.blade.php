d6d8e828284d56066abd9684140db653cd366e1c
Aleks <my@mail.com>
2022-09-30 20:58:01 +0300

    add table data-test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,23 +4,25 @@
     <div class="container-lg">
         <h1 class="mt-5 mb-3">Сайты</h1>
         <div class="table-responsive">
-            <table class="table table-bordered table-hover text-nowrap">
-                <tr>
-                    <th>ID</th>
-                    <th>Имя</th>
-                    <th>Последняя проверка</th>
-                    <th>Код ответа</th>
-                </tr>
-                @foreach ($urls as $url)
+            <table class="table table-bordered table-hover text-nowrap" data-test="urls">
+                <tbody>
                     <tr>
-                        <th>{{ $url->id }}</th>
-                        <th><a href="{{route('urls.show', $url->id)}}">{{ $url->name }}</a></th>
-                        <th>{{ $latestCheck->firstWhere('url_id', $url->id)->status_code ?? ''}}</th>
-                        <th>{{ $latestCheck->firstWhere('url_id', $url->id)->last_check_created_at ?? '' }}</th>
+                        <th>ID</th>
+                        <th>Имя</th>
+                        <th>Последняя проверка</th>
+                        <th>Код ответа</th>
                     </tr>
-                @endforeach
+                    @foreach ($urls as $url)
+                        <tr>
+                            <td>{{ $url->id }}</td>
+                            <td><a href="{{ route('urls.show', $url->id) }}">{{ $url->name }}</a></td>
+                            <td>{{ $latestCheck->firstWhere('url_id', $url->id)->last_check_created_at ?? '' }}</td>
+                            <td>{{ $latestCheck->firstWhere('url_id', $url->id)->status_code ?? '' }}</td>
+                        </tr>
+                    @endforeach
+                <tbody>
             </table>
-            
+
         </div>
         {{ $urls->links() }}
     </div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,19 +5,21 @@
     <div class="container-lg">
         <h1 class="mt-5 mb-3">Сайт: {{ $url->name }}</h1>
         <div class="table-responsive">
-            <table class="table table-bordered table-hover text-nowrap">
-                <tr>
-                    <td>ID</td>
-                    <td>{{ $url->id }}</td>
-                </tr>
-                <tr>
-                    <td>Имя</td>
-                    <td>{{ $url->name }}</td>
-                </tr>
-                <tr>
-                    <td>Дата создания</td>
-                    <td>{{ $url->created_at }}</td>
-                </tr>
+            <table class="table table-bordered table-hover text-nowrap" data-test="url">
+                <tbody>
+                    <tr>
+                        <td>ID</td>
+                        <td>{{ $url->id }}</td>
+                    </tr>
+                    <tr>
+                        <td>Имя</td>
+                        <td>{{ $url->name }}</td>
+                    </tr>
+                    <tr>
+                        <td>Дата создания</td>
+                        <td>{{ $url->created_at }}</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
         <h2 class="mt-5 mb-3">Проверки</h2>
@@ -26,24 +28,26 @@
             <input type="submit" class="btn btn-primary" value="Запустить проверку">
         </form>
         <table class="table table-bordered table-hover text-nowrap" data-test="checks">
-            <tr>
-                <th>ID</th>
-                <th>Код ответа</th>
-                <th>h1</th>
-                <th>title</th>
-                <th>description</th>
-                <th>Дата создания</th>
-            </tr>
-            @foreach ($checks as $check)
+            <tbody>
                 <tr>
-                    <td>{{ $check->id }}</td>
-                    <td>{{ $check->status_code }}</td>
-                    <td>{{ $check->h1 }}</td>
-                    <td>{{ $check->title }}</td>
-                    <td>{{ $check->description }}</td>
-                    <td>{{ $check->created_at }}</td>
+                    <th>ID</th>
+                    <th>Код ответа</th>
+                    <th>h1</th>
+                    <th>title</th>
+                    <th>description</th>
+                    <th>Дата создания</th>
                 </tr>
-            @endforeach
+                @foreach ($checks as $check)
+                    <tr>
+                        <td>{{ $check->id }}</td>
+                        <td>{{ $check->status_code }}</td>
+                        <td>{{ $check->h1 }}</td>
+                        <td>{{ $check->title }}</td>
+                        <td>{{ $check->description }}</td>
+                        <td>{{ $check->created_at }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
 @endsection
